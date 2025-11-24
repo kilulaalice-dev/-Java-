4d29b27f3fa9c34809840644e62eb2680bd3b77d
kilulaalice-dev <kilulaalice@gmail.com>
2025-11-25 00:32:52 +0300

    Create vacation.......cost.java

---
+++ vacation.......cost.java
@@ -0,0 +1,18 @@
+Public class vacation packageprice(
+          Public static double 
+CalculateTotalCost 
+          (Double accommodationcost)
+,doubmealcost, double activitycost)
+  
+         {return accommodationcost+mealcost+activitycost;}
+   {Public static void main(string[ ] 
+   args}
+    double accommodation =input.
+next double ( );
+double meal = input. next double ( );
+next activity=input. next double ( );
+
+Double total =  calculatetotalcost(Doubleaccomodationcost, doubmealcost, double activitycost ( );
+System.out.printin ( Total vocational cost is $"=total) ;
+{
+}
